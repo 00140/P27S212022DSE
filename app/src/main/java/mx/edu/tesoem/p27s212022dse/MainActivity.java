902240c40541b6576e880b6e3777ecd24726b648
kiree <kire.erickoz@gmail.com>
2022-09-21 17:09:12 -0500

    jjj

---
+++ app/src/main/java/mx/edu/tesoem/p27s212022dse/MainActivity.java
@@ -0,0 +1,14 @@
+package mx.edu.tesoem.p27s212022dse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
